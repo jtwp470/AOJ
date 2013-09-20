d227c5a7b124aef57caf3a0cef3dae569ad65942
Ryosuke Sato <rskjtwp@gmail.com>
2013-09-20 18:15:58 +0900

    10006 adds

---
+++ AOJ/PrintTestCases.java
@@ -0,0 +1,26 @@
+/*
+ * AOJ Volume 100 10006 Print Test Cases
+ */
+import java.io.*;
+
+class PrintTestCases {
+    public static void main(String args[]){
+	int i = 0;
+	int a = 0;
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));	
+	try{
+	    String line = reader.readLine();
+	    a = Integer.parseInt(line);
+	    while(a != 0){
+		System.out.println("Case "+(i+1)+": "+a);	    		
+		i++;
+		line = reader.readLine();
+		a = Integer.parseInt(line);
+	    }
+	}catch(IOException e){
+	    System.out.println(e);
+	}catch(Exception e){
+	    System.out.println(e);
+	}
+   }
+}
