837af2e64bfddef3d9869c444d36f4f5ccf09672
Ryosuke Sato <rskjtwp@gmail.com>
2013-09-20 16:54:53 +0900

    AOJ 10003

---
+++ AOJ/SmallLargeorEqual.java
@@ -0,0 +1,25 @@
+/*
+ * AOJ Volume 100 10003 Small,Large, or Equal
+ */
+import java.io.*;
+import java.util.*;
+class SmallLargeorEqual {
+    public static void main(String args[]){
+	int a, b;
+	try{
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	    String str = reader.readLine();
+	    StringTokenizer st = new StringTokenizer(str, " ");
+	    a = Integer.parseInt(st.nextToken());
+	    b = Integer.parseInt(st.nextToken());
+	    
+	    if(a > b) System.out.println(a+" > "+b);
+	    else if(a < b) System.out.println(a+" < "+b);
+	    else if(a == b) System.out.println(a+" == "+b);
+	}catch(IOException e){
+	    System.out.println(e);
+	}catch(Exception e){
+	    System.out.println(e);
+	}
+    }
+}
