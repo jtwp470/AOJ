b9886c8e88717ce85e4dddc97b90db3922253141
Ryosuke Sato(on Ub 13.10) <rskjtwp@gmail.com>
2013-11-19 22:39:22 +0900

    Complete RPN.java (Accepted)

--- lecture/03_ElementaryDataStructures/RPN.java
+++ lecture/03_ElementaryDataStructures/RPN.java
@@ -1,53 +1,58 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.Scanner;
 public class RPN{
     public static void main(String[] args) {
 	// スタック
-	IntStack is = new IntStack(50);
-	String line = "";
+	IntStack is = new IntStack(100);
 	int len = 0;
-	Scanner sc = new Scanner(System.in);
+	int space = 0;
+	String input[];
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	try{
-	    line = br.readLine();
-	    len = line.length();
+	    String line = br.readLine();
+	    input = line.split("\\s");
+	    len = input.length;
+	    for (int i = 0; i < len; i++) {
+		// 文字列が数字かどうかを判定
+		if(isDigit(input[i])){
+		    // 数字なのでスタックにプッシュ
+		    is.push(Integer.parseInt(input[i]));
+		    System.out.println(i+": push to stack:"+Integer.parseInt(input[i]));
+		}else{
+		    // 数字でないということは演算子なので計算する
+		    if(input[i].equals("+")){
+			int m = is.pop();
+			int n = is.pop();
+			is.push(m+n);
+			System.out.println(i+": push to stack:"+(m+n));
+		    }else if(input[i].equals("-")){
+			int m = is.pop();
+			int n = is.pop();
+			// LIFOなので引く順番が逆
+			System.out.println(m+":"+n);
+			is.push(n-m);
+		    }
+		    else if(input[i].equals("*")){
+			int m = is.pop();
+			int n = is.pop();
+			System.out.println(m+":"+n);
+			is.push(m*n);
+		    }
+		}		
+	    }
+	// 結果を示す(最上段をポップ)
+	System.out.println(is.pop());
+
 	}catch(IOException e){
 	    e.printStackTrace();
 	}
-	for (int i = 0; i < len; i++) {
-	    char c = line.charAt(i);
-	    // 文字列が数字かどうかを判定
-	    if(isDigit(c)){
-		// 数字なのでスタックにプッシュ
-		is.push(Integer.parseInt(String.valueOf(c)));
-	    }else{
-		// 数字でないということは演算子なので計算する
-		if(c == '+'){
-		    int m = is.pop();
-		    int n = is.pop();
-		    is.push(m+n);
-		}else if(c == '-'){
-		    int m = is.pop();
-		    int n = is.pop();
-		    // LIFOなので引く順番が逆
-		    is.push(n-m);
-		}
-		else if(c == '*'){
-		    int m = is.pop();
-		    int n = is.pop();
-		    is.push(m*n);
-		}
-	    }
-	}
-	// 結果を示す(最上段をポップ)
-	System.out.println(is.pop());
+	
     }
     //--- 文字列が数字かどうかを判定するメソッド ---//
-    public static boolean isDigit(Character s){
+    public static boolean isDigit(String s){
 	try{
-	    Integer.parseInt(String.valueOf(s));
+	    Integer.parseInt(s);
 	    return true;
 	}catch(NumberFormatException e){
 	    return false;
