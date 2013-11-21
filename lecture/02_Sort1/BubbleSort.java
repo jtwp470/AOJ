2127c4553eaae72f982dad14330b6c409b2261c9
Ryosuke Sato (MacBook Air) <rskjtwp@gmail.com>
2013-11-21 17:52:51 +0900

    Fix BubbleSort.java and solve until test case 5

--- lecture/02_Sort1/BubbleSort.java
+++ lecture/02_Sort1/BubbleSort.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
 class BubbleSort{
     public static void main(String args[]){
-	int ch = 0;
 	Scanner sc = new Scanner(System.in);
-	int n = sc.nextInt();
-	int x[] = new int[n];
-	for(int i=0; i<n; i++)
-	    x[i] = sc.nextInt();
-	for(int i=0; i<n-1; i++)
-	    for(int j=i+1; j<n; j++)
-		if(x[i]>x[j]){
-		    int tmp = x[i];
-		    x[i] = x[j];
-		    x[j] = tmp;
-		    ch++;
+	int x[];
+	int num = sc.nextInt();
+	try{
+	    x = new int[num];
+	    for (int i = 0; i < num; i++) 
+		x[i] = sc.nextInt();
+	    int t = bubbleSort(x, num);
+	    System.out.println(t);
+	}catch(Exception e){
+	    e.printStackTrace();
+	}	
+    }
+    public static int bubbleSort(int a[],int n){
+	int cnt = 0;
+	for (int i = 0; i < n-1; i++) {
+	    for (int j = i+1; j < n; j++) {
+		if(a[i]>a[j]){
+		    swap(a,i,j);
+		    cnt++;
 		}
-	for(int i=0; i<n-1; i++)
-	    System.out.print(x[i]+" ");
-	System.out.println(x[x.length-1]);
-	System.out.println(ch);	    
+	    }
+	}
+	for (int i = 0; i < n; i++) 
+	    System.out.print(a[i]+" ");	
+	System.out.println();
+	return cnt;
+    }
+    public static void swap(int a[], int idx1, int idx2){
+	int tmp = a[idx1];	a[idx1] = a[idx2];	a[idx2] = tmp;
     }
 }
