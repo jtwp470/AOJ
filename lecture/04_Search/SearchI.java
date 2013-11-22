847d919d22caf06869fc044d9f6542e585854809
Ryosuke Sato (MacBook Air) <rskjtwp@gmail.com>
2013-11-22 22:52:49 +0900

    New SearchI.java

---
+++ lecture/04_Search/SearchI.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class SearchI {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int tmp[] = new int[10000];
+	int ptr = 0;
+	int cnt = 0;
+	int num1 = sc.nextInt();
+	int s[] = new int[num1];
+	for (int i = 0; i < num1; i++) 
+	    s[i] = sc.nextInt();
+	int num2 = sc.nextInt();
+	int t[] = new int[num2];
+	for (int i = 0; i < num2; i++) 
+	    t[i] = sc.nextInt();
+
+	for (int i = 0; i < num1; i++){ 
+	    for (int j = 0; j < num2; j++){ 
+		cnt++;
+	    }
+	}
+	System.out.println(cnt);
+    }
+}
